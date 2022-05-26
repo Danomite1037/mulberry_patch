e0cca0c5d3dd903f58c51dd06040223c9160b0ab
Danny <dmcnemar1037@gmail.com>
2022-05-26 15:18:40 -0400

    first commit

--- backend/server/models/user.model.js
+++ backend/server/models/user.model.js
@@ -3,34 +3,28 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     firstName:{
         type : String,
-        required : [true, "All users must have a name"],
-        minLength: [3, "Name must be at least 3 Charactors"]
+        required : [true, "All Users must have a First Name"],
     },
     lastName:{
         type : String,
-        required :  [true, "All users must have a type"],
-        minLength: [3, "Type must be at least 3 Charactors"]
-        
+        required :  [true, "All Users must have a Last Name"],
     },
     email:{
         type : String,
-        required :  [true, "The user must be interesting enough to say somthung about it"],
-        minLength: [3, "Description must be at least 3 Charactors"],
+        required :  [true, "All Users must enter an Email"],
         validate: {
             validator: val => /^([\w-\.]+@([\w-]+\.)+[\w-]+)?$/.test(val),
             message: "Please enter a valid email"
         }
     },
-    subscribe:{
-        type : Boolean, 
-        
-    },
     password:{
         type : String, 
-        required : [true, "Dont forget to make your password."],
+        required : [true, "Dont forget to create your password."],
         minLength : [8, "Your password should be at least 8 charactors long"]
+    },
+    subscribe:{
+        type : Boolean
     }
-
 }, {timestamps:true})
 
 const user = mongoose.model ("user", userSchema);
